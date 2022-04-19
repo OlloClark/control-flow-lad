0767f752f4233efbeecb89d46aae8a0da871801e
OlloClark <ollo42@hotmail.co.uk>
2022-04-19 13:52:38 -0700

    ex 2 working

--- exercise-2.py
+++ exercise-2.py
@@ -7,3 +7,10 @@
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
+phrase = input("Please enter a word or phrase:")
+
+while phrase != "quit":
+    print(f"what you entered is {len(phrase)} characters long")
+    print("type 'quit' to exit loop")
+    phrase = input("Please enter a word or phrase:")
+
