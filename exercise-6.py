13d9d52916f06163a4807ee45fd40019e6cdd51c
OlloClark <ollo42@hotmail.co.uk>
2022-04-19 17:52:37 -0700

    ex 6 working

--- exercise-6.py
+++ exercise-6.py
@@ -19,3 +19,57 @@
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+
+month = input("Enter the month of the year (Jan - Dec):").upper()
+
+if month in ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]:
+    print(f"You entered {month}")
+    day = input("Enter the day of the month (1-31):")
+    if int(day) in range(1, 32):
+        print(f"you entered {month} {day}")
+    else:
+        print("not a valid day; use 1-31")
+    if month in ["JAN", "FEB"]:
+        if month == "FEB" and int(day) in range(29,32):
+            print("FEB only has 28 days")
+        else:
+            print(f"{month} {day} is in Winter")
+    elif month == "MAR":
+        if int(day) < 20:
+            print(f"{month} {day} is in Winter")
+        else:
+            print(f"{month} {day} is in Spring")
+    elif month in ["APR", "MAY"]:
+        if month == "APR" and int(day) == 31:
+            print(f"{month} only has 30 days")
+        else:
+            print(f"{month} {day} is in Spring")
+    elif month == "JUN":
+        if int(day) == 31:
+            print(f"{month} only has 30 days")
+        elif int(day) < 21:
+            print(f"{month} {day} is in Spring")
+        else:
+            print(f"{month} {day} is in Summer")
+    elif month in ["JUL", "AUG"]:
+        print(f"{month} {day} is in Summer")
+    elif month == "SEP":
+        if int(day) == 31:
+            print(f"{month} only has 30 days")
+        elif int(day) < 22:
+            print(f"{month} {day} is in Summer")
+        else:
+            print(f"{month} {day} is in Autumn")
+    elif month in ["OCT", "NOV"]:
+        if month == "NOV" and int(day) == 31:
+            print(f"{month} only has 30 days")
+        else:
+            print(f"{month} {day} is in Autumn")
+    elif month == "DEC":
+        if int(day) < 21:
+            print(f"{month} {day} is in Autumn")
+        else:
+            print(f"{month} {day} is in Winter")
+else:
+    print("not a valid entry; use 3 letter month code")
+
